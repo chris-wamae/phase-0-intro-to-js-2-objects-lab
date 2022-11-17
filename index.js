659a6b8f66535d8cd688d0629e7419b13d2cebc4
Chris Wamae <105814303+chris-wamae@users.noreply.github.com>
2022-11-17 16:15:34 +0300

    "Completed Assignment"

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {name:"Joe",
+                  streetAddress:1}
+                
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{const employeeCopy = { ...employee };
+
+employeeCopy[key] = value;
+
+return employeeCopy;}
+
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value)
+{   employee["name"] ="Sam";
+    employee["streetAddress"] ="12 Broadway"
+    
+    return employee}
+
+function deleteFromEmployeeByKey(employee, key)
+{const noKeyEmployee = {...employee}
+ delete noKeyEmployee["name"]
+ return noKeyEmployee}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{ delete employee["name"]
+return employee}
